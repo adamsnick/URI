0af931ea58de6d1d889fb0658016394c0569914e
adamsnick <81978698+adamsnick@users.noreply.github.com>
2021-07-06 12:24:22 -0300

    Create 1042.c

---
+++ 1042.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    
+    scanf("%d%d%d",&A,&B,&C);
+    
+        if(A > B && A > C){
+            if (B > C){
+                printf("%d\n%d\n%d\n",C,B,A);
+            }else{
+                printf("%d\n%d\n%d\n",B,C,A);
+            }
+        }else if(B > A && B > C){
+            if(A > C){
+                printf("%d\n%d\n%d\n",C,A,B);
+            }else{
+                printf("%d\n%d\n%d\n",A,C,B);
+            }
+        }else if(C > A && C > B){
+            if(A > B){
+                printf("%d\n%d\n%d\n",B,A,C);   
+            }else{
+                printf("%d\n%d\n%d\n",A,B,C);
+            }
+        }
+        printf("\n");
+        printf("%d\n%d\n%d\n",A,B,C);
+        
+        
+    return 0;
+}
