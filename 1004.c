f87a60a6cfe591ad36ff98b0da69e7184c4a08a1
adamsnick <81978698+adamsnick@users.noreply.github.com>
2021-07-10 14:17:16 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{   
+    int PROD, valor1, valor2;
+    
+    scanf("%d%d",&valor1,&valor2);
+    PROD = valor1 * valor2;
+    printf("PROD = %d\n",PROD);
+    
+    return 0;
+}
