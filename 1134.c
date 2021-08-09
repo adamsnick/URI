58f08e3d2d6c875d5411e2a81afd8862987ba6ba
adamsnick <81978698+adamsnick@users.noreply.github.com>
2021-08-08 21:55:14 -0300

    Create 1134.c

---
+++ 1134.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int codigo=0,alcool=0,gasolina=0,diesel=0;
+    
+    while(codigo!=4){
+        scanf("%d",&codigo);
+            if(codigo==1){
+                alcool++;
+            }else if(codigo==2){
+                gasolina++;
+            }else if(codigo==3){
+                diesel++;
+            }
+    }
+    
+        printf("MUITO OBRIGADO\n");
+        printf("Alcool: %d\n",alcool);
+        printf("Gasolina: %d\n",gasolina);
+        printf("Diesel: %d\n",diesel);
+        
+    return 0;
+}
